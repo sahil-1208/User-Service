6e19677dcea2c885718eed5df7e983bb2d835b6a
Sahil Rajpal <sahilrajpal968@gmail.com>
2024-02-21 16:54:19 +0530

    First Push

---
+++ dating-app-service/src/main/java/com/learning/DatingAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAppServiceApplication.class, args);
+	}
+
+}
